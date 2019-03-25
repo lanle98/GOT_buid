a1def5676e367022c12db8d09061258ea0666124
Darren Le <darrenle98@gmail.com>
2019-03-25 12:49:59 -0400

    changes

--- js/GOT_master.js
+++ js/GOT_master.js
@@ -7,16 +7,24 @@
 			video = document.querySelector('video')
 			lbClose = document.querySelector(".lightbox-close")
 	    	topBanners = document.querySelector("#houseImages")
+	    	tagline = document.querySelector('.house-name')
+	    	houseInfo = document.querySelector('.house-info')
+
+	const houseData = [
+		["stark", `House Stark of Winterfell is a Great House of Westeros, ruling over the vast region known as the North from their seat in Winterfell. It is one of the oldest lines of Westerosi nobility by far, claiming a line of descent stretching back over eight thousand years. Before the Targaryen conquest, as well as during the War of the Five Kings and Daenerys Targaryen's invasion of Westeros, the leaders of House Stark ruled over the region as the Kings in the North.`],
+		["BARATHEON", `House Baratheon of Storm's End is a legally extinct Great House of Westeros. A cadet branch was formerly the royal house, but House Lannister now controls the throne. House Baratheon traditionally ruled the Stormlands on the eastern coast of Westeros, aptly named for its frequent storms, from their seat of Storm's End.`],
+		["LANNISTER", `House Lannister of Casterly Rock is one of the Great Houses of Westeros, one of its richest and most powerful families and oldest dynasties. It is also the current royal house of the Seven Kingdoms following the extinction of House Baratheon of King's Landing, which had been their puppet house anyway.`],
+	]
 
 	function openLightbox(){
 		//debugger;
 
-		let targetHouse = sigil.className.split(" ")[1];
+		let targetHouse = this.className.split(" ")[1];
 
 		//this gives us a lowercase house name -> the second class on 
 		//all of the shields
 
-
+		
 
 		//flip this to uppercase
 		let targetVid = targetHouse.charAt(0).toUpperCase() + targetHouse.slice(1);
@@ -44,16 +52,17 @@
 		const offSet = 600;
 		let currentOffset = this.dataset.offset * offSet;
 
+		tagline.innerHTML = `House ${houseData[this.dataset.offset][0]}` ;
+		houseInfo.innerHTML = houseData[this.dataset.offset][1]
 
 		TweenMax.to(topBanners,0.8, {
-		right: currentOffset
-
-
-
-	});
+		right: currentOffset});
 		// topBanners.style.right = currentOffset + "px";
-		// sigils.forEach(sigil => sigil.addEventListener('click', ))
-		video.setTimeout(openLightbox,1000)
+
+
+		// change the text content on the page per house
+		
+		
 	
 		
 	}
